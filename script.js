d840733a1a0cfab00cd82a90c5f8b7bcc2fb3853
yeak558 <yeak558@gmail.com>
2026-02-19 22:16:03 +0300

    Add basic property, right now only 1 operation at a time

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
 
 // Init variables
-let operandRight;
-let operandLeft;
+let operandFirst;
+let operandSecond;
 let operator;
 
 let dummy;
 
+const operations = "+-*/";
 
 buttons.forEach(
 	function (node) {
 		node.addEventListener( "click", (event) => {
 			dummy = node.textContent;
+			// Display part
 			switch (dummy) {
 				case "CLR":
 					screen.textContent = "";
@@ -23,27 +25,42 @@ buttons.forEach(
 					break;
 				default:
 					screen.textConten += ` ${dummy}`;
-				screen.textContent += ` ${dummy}`;
+					screen.textContent += ` ${dummy}`;
+			}
+			// Input handling part
+			if (operations.includes(dummy)) {
+				operator = dummy;
+			} else if (operandFirst === undefined) {
+				operandFirst = dummy;
+			} else if (operandSecond === undefined) {
+				operandSecond = dummy;
+			} else if (dummy === "CLR"){
+				operandFirst = undefined;
+				operandSecond = undefined;
+				operator = undefined;
+			} else {
+				let result = operate(operator, Number(operandFirst), Number(operandSecond));
+				screen.textContent = result;
 			}
 		});
 	}
 
 )
 
-function operate(operator, operandRight, operandLeft) {
+function operate(operator, operandFirst, operandSecond) {
 	let result;
 	switch (operator) {
 		case "+":
-			result = add(operandRight, operandLeft);
+			result = add(operandFirst, operandSecond);
 			break;
 		case "-":
-			result = subtract(operandRight, operandLeft);
+			result = subtract(operandFirst, operandSecond);
 			break;
 		case "*":
-			result = multiply(operandRight, operandLeft);
+			result = multiply(operandFirst, operandSecond);
 			break;
 		case "/":
-			result = divide(operandRight, operandLeft);
+			result = divide(operandFirst, operandSecond);
 			break;
 		default:
 			console.log("Undefined operator");
