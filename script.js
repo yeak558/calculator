d66c5c683712b2919ec5bd587f46cf8b2c812899
yeak558 <yeak558@gmail.com>
2026-02-19 21:30:47 +0300

    Add operate function

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ let operandRight;
 let operandLeft;
 let operator;
 
-
+function operate(operator, operandRight, operandLeft) {
+	let result;
+	switch (operator) {
+		case "+":
+			result = add(operandRight, operandLeft);
+			break;
+		case "-":
+			result = subtract(operandRight, operandLeft);
+			break;
+		case "*":
+			result = multiply(operandRight, operandLeft);
+			break;
+		case "/":
+			result = divide(operandRight, operandLeft);
+			break;
+		default:
+			console.log("Undefined operator");
+	}
+	return result;
+}
 
 function add(a, b) {
 	return a + b;
